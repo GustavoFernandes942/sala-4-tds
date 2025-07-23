c928d18dcb979ad047ca68cd0bd49471dca883cd
GustavoFernandes942 <gustavo.carneiro01092334@sesisenaipr.org.br>
2025-07-23 10:53:06 -0300

    Add files via upload

--- sala-4-tds/script.js
+++ sala-4-tds/script.js
@@ -4,19 +4,25 @@ function soma(){
     document.getElementById('respsoma').textContent = numero1 + numero2
 }
 
-function divisao(){
-    var numero1 = document.getElementById("n3").valueAsNumber
-    var numero2 = document.getElementById("n4").valueAsNumber
-    document.getElementById('respdiv').textContent = numero1 / numero2
+function div(){
+    var numero1 = parseFloat(document.getElementById("n3").value)
+    var numero2 = parseFloat(document.getElementById("n4").value)
+    var resposta = document.getElementById('respdiv')
+    if (numero2 !==0){
+        resposta.innerHTML = numero1 / numero2
+    }else{
+        resposta.textContent = "nao se divide por 0"
+    }
 }
 
-function multiplicação(){
-    var numero1 = document.getElementById("n5").valueAsNumber
-    var numero2 = document.getElementById("n6").valueAsNumber
+
+function mult(){
+    var numero1 = parseFloat(document.getElementById("n5").value)
+    var numero2 = parseFloat(document.getElementById("n6").value)
     document.getElementById('respmult').textContent = numero1 * numero2
 }
 
-function subtracao(){
+function sub(){
     var numero1 = document.getElementById("n7").valueAsNumber
     var numero2 = document.getElementById("n8").valueAsNumber
     document.getElementById('respsub').textContent = numero1 - numero2
